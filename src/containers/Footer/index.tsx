6f2dd1d300fcc5859acc6c0605075b065ef15e38
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2021-08-29 19:34:55 +0300

    Update index.tsx

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -12,7 +12,7 @@ class FooterComponent extends React.Component<RouterProps> {
             <React.Fragment>
                 <footer className="pg-footer">
                     <span>Powered by</span>
-                    <a href="https://www.openware.com">openware.com</a>
+                    <a href="https://www.mesh.llc">Mesh.llc</a>
                 </footer>
             </React.Fragment>
         );
