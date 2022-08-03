29da1e32c5892b76020a2debe5eef47ea4fc1da6
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2022-08-04 02:09:31 +0300

    Update tr.ts

--- src/custom/translations/tr.ts
+++ src/custom/translations/tr.ts
@@ -1,7 +1,7 @@
 import { LangType } from '../../translations';
 import { nationalitiesNames } from '../../translations/nationalities';
 
-export const ru: LangType = {
+export const tr: LangType = {
     'page.header.navbar.signIn': 'kayıt ol',
     'page.header.navbar.trade': 'Ticaret',
     'page.header.navbar.wallets': 'Cüzdanlar',
