fea7647f472aff2c0f17267d641bc940231cd7ed
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2022-08-15 03:23:21 +0300

    Create tr.ts

---
+++ src/mobile/translations/tr.ts
@@ -0,0 +1,119 @@
+export const tr = {
+    'page.mobile.header.signIn': 'Giriş yap',
+    'page.mobile.footer.home': 'Anasayfa',
+    'page.mobile.footer.orders': 'sipariş',
+    'page.mobile.footer.trading': 'Trade',
+    'page.mobile.footer.wallets': 'Cüzdanlar',
+    'page.mobile.footer.profile': 'Profil',
+
+    'page.mobile.tradingTabs.label.createOrder': 'Sipariş Oluştur',
+    'page.mobile.tradingTabs.label.charts': 'Grafikler',
+    'page.mobile.tradingTabs.label.orders': 'sipariş',
+
+    'page.mobile.charts.label.orderBook': 'Sipariş defteri',
+    'page.mobile.charts.label.trades': 'Trades',
+
+    'page.mobile.order.price.placeholder': 'Fiyat ({para birimi})',
+    'page.mobile.order.amount.placeholder': 'Tutar ({para birimi})',
+
+    'page.mobile.orderButtons.buy': 'Satın al{ana ünite}',
+    'page.mobile.orderButtons.sell': 'Satma {ana ünite}',
+
+    'page.mobile.orders.tabs.open': 'Açık',
+    'page.mobile.orders.tabs.all': 'Hepsi',
+    'page.mobile.orders.cancelAll': 'Hepsini iptal et',
+
+    'page.mobile.orders.open.title': 'Açık siparişler',
+    'page.mobile.orders.header.orderType.buy.market': 'Pazar satın al',
+    'page.mobile.orders.header.orderType.buy.limit': 'Satın alma limiti',
+    'page.mobile.orders.header.orderType.sell.market': 'pazar sat',
+    'page.mobile.orders.header.orderType.sell.limit': 'Satış sınırı',
+    'page.mobile.orders.header.amount': 'Tutar',
+    'page.mobile.orders.header.price': 'Fiyat',
+    'page.mobile.orders.header.filled': 'Dolu',
+    'page.mobile.orders.header.cancel': 'İptal ',
+
+    'page.mobile.currentMarketInfo.search.placeholder': 'Ara',
+    'page.mobile.currentMarketInfo.volume': 'Ses',
+    'page.mobile.currentMarketInfo.high': 'Yüksek',
+    'page.mobile.currentMarketInfo.low': 'Düşük',
+
+    'page.header.signIn.forgotPassword': 'Parolanızı mı unuttunuz?',
+    'page.header.signIN.noAccountYet': 'Henüz hesap yok mu?',
+    'page.header.signUp.terms': 'Hizmet açısından tüm ifadeleri kabul ediyorum',
+    'page.header.signUp.alreadyRegistered': 'Zaten kayıtlı?',
+
+    'page.mobile.reset.header': 'Doğrulamayı tekrar gönder',
+    'page.mobile.signin.kyc.header': '2FA Doğrulaması',
+
+    'page.mobile.wallets.deposit.history': 'Para yatırma geçmişi',
+    'page.mobile.wallets.withdraw.history': 'Geçmişi geri çekme',
+    'page.mobile.wallets.banner.total': 'Toplam',
+    'page.mobile.wallets.banner.available': 'Mevcut',
+
+    'page.mobile.copy.text': 'Adresi kopyala',
+    'page.mobile.wallet.deposit.generate': 'Para yatırma adresi oluştur',
+    'page.mobile.wallet.deposit.generating': 'adres oluşturuluyor...',
+
+    'page.mobile.wallet.withdraw.modal.new.account': 'Yeni hesabı onayla',
+    'page.mobile.wallet.withdraw.modal.confirmation': 'Geri çekme onayı',
+    'page.mobile.wallet.withdraw.modal.confirmation.warning': 'Lütfen para çekme işlemini onaylamadan önce hedef adresi dikkatlice kontrol edin..',
+    'page.mobile.wallet.withdraw.modal.confirmation.message1': 'Geri çekme talebinde bulundunuz',
+    'page.mobile.wallet.withdraw.modal.confirmation.message2': 'bu adrese',
+
+    'page.mobile.userInfo.details.uid': 'UID: {uid}',
+    'page.mobile.userInfo.logout.button': 'Çıkış Yap',
+    'page.mobile.profileLinks.main.verification': 'Profil doğrulama',
+    'page.mobile.profileLinks.main.2fa': 'İki faktörlü kimlik doğrulama',
+    'page.mobile.profileLinks.main.changePassword': 'Şifre değiştir',
+    'page.mobile.profileLinks.main.activity': 'Hesap haraketliliği',
+    'page.mobile.profileLinks.main.apiKeys': 'API anahtarlarım',
+
+    'page.mobile.profileLinks.settings.language': 'Dil',
+    'page.mobile.profileLinks.settings.theme': 'Tema',
+
+    'page.mobile.profileLinks.additional.referral': 'Yönlendirme Bağlantısı',
+    'page.mobile.profileLinks.link.referral.success': 'Başarıyla kopyalandı!',
+
+    'page.mobile.profileLinks.link.verification': 'Seviye {seviye}',
+    'page.mobile.profileLinks.link.2fa.enabled': 'Etkinleştirilmiş',
+    'page.mobile.profileLinks.link.2fa.disabled': 'Engelli',
+    'page.mobile.profileLinks.link.referral': 'Bağlantıyı kopyala',
+
+    'page.mobile.profileColorTheme.theme.dark': 'koyu',
+    'page.mobile.profileColorTheme.theme.light': 'Işık',
+
+    'page.mobile.wallets.title': 'Cüzdanlar',
+
+    'page.mobile.profile.accountActivity.title': 'Hesap haraketliliği',
+    'page.mobile.profile.apiKeys.title': 'API anahtarlarım',
+    'page.mobile.profile.theme.title': 'Tema',
+    'page.mobile.profile.language.title': 'Dil',
+    'page.mobile.profile.changePassword.title': 'Şifre değiştir',
+
+    'page.mobile.profile.accountActivity.action': 'Eylem',
+    'page.mobile.profile.accountActivity.ip': 'IP adres',
+    'page.mobile.profile.accountActivity.result': 'Sonuç',
+
+    'page.mobile.profile.apiKeys.item.kid': 'Çocuk',
+    'page.mobile.profile.apiKeys.item.algorithm': 'algoritma',
+    'page.mobile.profile.apiKeys.item.created': 'oluşturuldu',
+    'page.mobile.profile.apiKeys.item.updated': 'Güncellenmiş',
+    'page.mobile.profile.apiKeys.item.delete': 'Sil',
+
+    'page.mobile.confirm.profile': 'Kimlik doğrulama',
+    'page.mobile.confirm.document': 'kimlik doğrulama',
+    'page.mobile.confirm.address': 'ikamet doğrulama',
+    'page.mobile.confirm.back': 'Geri',
+
+    'page.body.profile.header.account.content.twoFactorAuthentication':  'İki faktörlü kimlik doğrulama',
+
+    'page.profile.kyc.title': '2FA',
+
+    'page.mobile.twoFactorModal.title': '2FA Doğrulaması',
+    'page.mobile.twoFactorModal.subtitle': 'Uygulamadan 2fa kodunu girin',
+    'page.mobile.twoFactorModal.send': 'Gönder',
+
+    'page.mobile.createdApiKeyModal.title': 'oluşturuldu',
+    'page.mobile.createdApiKeyModal.confirm': 'onayla',
+};
